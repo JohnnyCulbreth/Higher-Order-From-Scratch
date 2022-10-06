ea24eed576f573e3957bb2433e77397d2f2d808f
JohnnyCulbreth <johnnyculbrethjr@yahoo.com>
2022-10-05 19:38:45 -0500

    started todos

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-// .map() - function that takes an array of items and a function that returns an array with each item manipulated by that function.
+// 1. .map() - function that takes an array of items and a function that returns an array with each item manipulated by that function.
+
+const numbers = [2, 42, 99, 882];
+const newArr = numbers.map(timesTwo)
+
+function timesTwo(num) {
+  return num * 2;
+}
+
+console.log(newArr)
+
+// (Multiplies all the values in an array by 2!)
+
+
+// 2. .reduce() - function that takes an array of food orders with their amounts and returns the total amount of all the food orders.
 
 
 
-// .reduce() - function that takes an array of food orders with their amounts and returns the total amount of all the food orders.
-
-
-
-// .filter() - function that takes an array of items and a function that returns an array with only the items that return true in the function.
+// 3. .filter() - function that takes an array of items and a function that returns an array with only the items that return true in the function.
