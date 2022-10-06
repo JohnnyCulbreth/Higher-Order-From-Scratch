b1935fb204d46cd56bded211c3cb22ea5e972fd6
Hoogachaka <thatdylan@gmail.com>
2022-10-05 22:31:04 -0500

    finished filter and cleaned up code

--- main.js
+++ main.js
@@ -1,34 +1,27 @@
 // 1. .map() - function that takes an array of items and a function that returns an array with each item manipulated by that function.
-
-// const newArr = numbers.map(timesTwo)
-
-// function timesTwo(num) {
-  //   return num * 2;
-  // }
-  
-  // console.log(newArr)
-
-  
+ 
   const arr1 = [2, 42, 99, 882];
   const arr2 = [1, 2, 3, 4]
-  let newArray = []
-
+  
   const timesTwo = (element) => {
     let newElement = element*2
-    // console.log(`New element: ${newElement}, ${typeof(newElement)}`)
     return newElement
   }
-
+  
   const mapFunction = (x, y) => {
+    let newArray = []
     for (let i = 0; i < y.length; i++) {
       newArray.push(x(y[i]))
-      }
-      console.log(newArray)
+    }
+    console.log(newArray)
+    return newArray
   }
   
-  const foodArray = [{'pizza': 10}, {'hotdog': 3.50},'salad', 'candy']
+  const foodArray = [{'pizza': 10}, {'hotdog': 3.50},{'salad': 6.25}, {'candy': 1.75}]
+  
+   mapFunction(timesTwo, arr1)
 
-  // mapFunction(timesTwo, arr1)
+  // 2. .reduce() - function that takes an array of food orders with their amounts and returns the total amount of all the food orders.
 
   const reduceFunction = (array) => {
     let ans = 0
@@ -38,16 +31,8 @@
     console.log(ans)
   }
 
-  // reduceFunction(foodArray)
-// reduceFunction(arr1)
-// reduceFunction(arr2)
-
-// (Multiplies all the values in an array by 2!)
-
-
-// 2. .reduce() - function that takes an array of food orders with their amounts and returns the total amount of all the food orders.
-
-
+reduceFunction(arr1)
+reduceFunction(arr2)
 
 // 3. .filter() - function that takes an array of items and a function that returns an array with only the items that return true in the function
 
@@ -62,10 +47,11 @@ const isGreater = (x) => {
   }
 }
 
-// add another if statement here
 const filterFunction = (x, y) => {
   for (let i = 0; i < y.length; i++) {
-    extraNewArray.push(x(y[i]));
+    if (x(y[i]) == true) {
+      extraNewArray.push(y[i]);      
+    }
     }
     console.log(extraNewArray)
 }
